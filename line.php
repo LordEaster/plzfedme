42c4cba7128f09153dd3c45337f2ee225f87ca45
marteiei <34980328+marteiei@users.noreply.github.com>
2019-09-15 15:37:18 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U1d8484c4c5ae201c75d27f6fd1d6370b',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
